4a8116d4ab5ade210465a5da33a0d5125f741964
nathan.mcculloch <nathan.mcculloch@otainsight.com>
2024-02-13 15:25:11 +0000

    version 3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, this is nathan speaking... this is version 2"
+    return "This is just a test version #3"
 
 
 if __name__ == "__main__":
